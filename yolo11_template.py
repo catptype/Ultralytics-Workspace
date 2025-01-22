4590bec581dff3e2b0f21c49fedc9d1d89ab5b69
Cat <prasertsakul.p@gmail.com>
2025-01-22 10:45:13 +0700

    Update yolo11_template.py

--- yolo11_template.py
+++ yolo11_template.py
@@ -3,24 +3,28 @@ from ultralytics import YOLO
 from util.Config import TRAIN_CONFIG_PRESET
 from util.Augmentation import AUGMENTATION_PRESET
 
-TASK = 'classify'
-DATASET = ''
-AUGMENT_SELECTION = 'none'
-APPLICATION = 'default'
-SEED = 0
+# EDIT HERE
+TASK = 'classify'           # YOLO task type: ['classify', 'detect', 'pose', 'obb']
+MODEL_SIZE = 'n'            # YOLO model size: ['n', 's', 'm', 'l', 'x']
+DATASET = ''                # Name of the dataset in the 'datasets' directory
+AUGMENT_SELECTION = 'none'  # Select augmentation preset (see AUGMENTATION_PRESET for options)
+APPLICATION = 'application' # Application name, used for naming the model in the 'runs' directory
+SEED = 0                    # Random seed value for reproducibility
 
+# -------------------------
 BASE_MODEL = {
-    'classify': f'yolo11s-cls.pt',
-    'detect': f'yolo11s.pt',
-    'pose': f'yolo11n-pose.pt',
-    'obb': f'yolo11n-obb.pt',
+    'classify': f'yolo11{MODEL_SIZE}-cls.pt',
+    'detect': f'yolo11{MODEL_SIZE}.pt',
+    'pose': f'yolo11{MODEL_SIZE}-pose.pt',
+    'obb': f'yolo11{MODEL_SIZE}-obb.pt',
 }
 
 def main():
     # Print stdout
     print(f"Model: {BASE_MODEL[TASK]}")
+    print(f"Task: {TASK}")
     print(f"Dataset: {DATASET}")
-    print(f"Augmentation configuration: {AUGMENT_SELECTION}")
+    print(f"Augmentation: {AUGMENT_SELECTION}")
     print("")
 
     # Initialize
